73efcd10cbe40f2a504ade2f5de8875f6635a2c2
PedroDella <pedrocabraitza@gmail.com>
2025-10-23 23:09:36 -0300

    feat: adicionando tela de login e cadastro (#5)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import AuthPage from './components/pages/AuthPage/AuthPage';
+
+function App() {
+
+  return (
+    <div>
+      <AuthPage />
+    </div>
+  )
+}
+
+export default App
